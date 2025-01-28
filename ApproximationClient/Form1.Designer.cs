44a2c9cd1c7508efddc0ee95028408b1873a91bf
Evgex <Evgex@DESKTOP-LQ2A3MO>
2025-01-28 15:45:40 +0300

    Добавьте файлы проекта.

---
+++ ApproximationClient/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace ApproximationClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button2 = new System.Windows.Forms.Button();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button1.Location = new System.Drawing.Point(28, 622);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(270, 95);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Загрузить данные из файла";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(9, 55);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Name = "Исходные значение";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Name = "Аппроксимация";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(568, 455);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button2.Location = new System.Drawing.Point(328, 622);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(270, 95);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Построить график";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend1.HeaderSeparatorColor = System.Drawing.Color.DarkGoldenrod;
+            legend1.Name = "Legend1";
+            this.chart2.Legends.Add(legend1);
+            this.chart2.Location = new System.Drawing.Point(574, 55);
+            this.chart2.Name = "chart2";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Legend = "Legend1";
+            series3.Name = "Исходные значение";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Legend = "Legend1";
+            series4.Name = "Аппроксимация";
+            this.chart2.Series.Add(series3);
+            this.chart2.Series.Add(series4);
+            this.chart2.Size = new System.Drawing.Size(832, 455);
+            this.chart2.TabIndex = 4;
+            this.chart2.Text = "chart2";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(640, 529);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(766, 188);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Column1
+            // 
+            this.Column1.Frozen = true;
+            this.Column1.HeaderText = "Расход, кг/сек";
+            this.Column1.MinimumWidth = 8;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 150;
+            // 
+            // Column2
+            // 
+            this.Column2.Frozen = true;
+            this.Column2.HeaderText = "Температура, С";
+            this.Column2.MinimumWidth = 8;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 150;
+            // 
+            // Column3
+            // 
+            this.Column3.Frozen = true;
+            this.Column3.HeaderText = "Отклонение уровня, мм";
+            this.Column3.MinimumWidth = 8;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 150;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1518, 33);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ToolStripMenuItem
+            // 
+            this.ToolStripMenuItem.Name = "ToolStripMenuItem";
+            this.ToolStripMenuItem.Size = new System.Drawing.Size(105, 29);
+            this.ToolStripMenuItem.Text = "Сведенье";
+            this.ToolStripMenuItem.Click += new System.EventHandler(this.ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1518, 872);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Аппроксимация";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem;
+    }
+}
+
---
+++ ApproximationClient/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace ApproximationClient
+{
+    public partial class Form1 : Form
+    {
+        ServiceApproximation.ServiceApproximationClient client;
+
+        double[] paramA = null;
+        double[] paramB = null;
+        double[] paramC = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new ServiceApproximation.ServiceApproximationClient();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+               
+                try
+                {
+                    dialog.Filter = "Текстовые файлы|*.txt";
+                    if (dialog.ShowDialog() == DialogResult.OK)
+                    {
+
+                        var file = new StreamReader(dialog.FileName);
+                        String strAize = file.ReadLine();
+                        int size = int.Parse(strAize);
+                        paramA = new double[size];
+                        paramB = new double[size];
+                        paramC = new double[size];
+                        dataGridView1.Rows.Clear();
+                        foreach (var series in chart1.Series)
+                        {
+                            series.Points.Clear();
+                        }
+                        foreach (var series in chart2.Series)
+                        {
+                            series.Points.Clear();
+                        }
+                        for (int i = 0; i < size; i++)
+                        {
+                            String strBufer = file.ReadLine();
+                            double bufer = double.Parse(strBufer);
+                            paramA[i] = bufer;
+                        }
+                        for (int i = 0; i < size; i++)
+                        {
+                            String strBufer = file.ReadLine();
+                            double bufer = double.Parse(strBufer);
+                            paramB[i] = bufer;
+                        }
+                        for (int i = 0; i < size; i++)
+                        {
+                            String strBufer = file.ReadLine();
+                            double bufer = double.Parse(strBufer);
+                            paramC[i] = bufer;
+                        }
+
+                        for (int i = 0; i < size; i++)
+                        {
+                            dataGridView1.Rows.Add(paramA[i], paramB[i], paramC[i]);
+                        }
+                        file.Close();
+                    }
+                }catch (System.FormatException)
+                {
+                    MessageBox.Show("В файле данные представлены в неправильном формате!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool validation = false;
+            do
+            {
+                try
+                {
+                    for (int i = 0; i < paramA.Length; i++)
+                    {
+                        paramA[i] = double.Parse(dataGridView1.Rows[i].Cells[0].Value.ToString());
+                        paramB[i] = double.Parse(dataGridView1.Rows[i].Cells[1].Value.ToString());
+                        paramC[i] = double.Parse(dataGridView1.Rows[i].Cells[2].Value.ToString());
+                        if (paramA[i] < 0)
+                        {
+                            validation = true;
+                            break;
+                        }
+                        if (paramB[i] < -273)
+                        {
+                            validation = true;
+                            break;
+                        }
+                    }
+                    foreach (var series in chart1.Series)
+                    {
+                        series.Points.Clear();
+                    }
+                    foreach (var series in chart2.Series)
+                    {
+                        series.Points.Clear();
+                    }
+
+                    if (validation)
+                    {
+                        MessageBox.Show("Расход не может быть отрицательным, " +
+                                            "отредактируйте данные и попробуйте снова!",
+                                            "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        break;
+                    }
+                    Axis ax = new Axis();
+                    ax.Title = "Расход, кг/сек";
+                    chart1.ChartAreas[0].AxisX = ax;
+                    Axis ay = new Axis();
+                    ay.Title = "Температура, С";
+                    chart1.ChartAreas[0].AxisY = ay;
+
+                    Axis ax2 = new Axis();
+                    ax2.Title = "Расход, кг/сек";
+                    chart2.ChartAreas[0].AxisX = ax2;
+                    Axis ay2 = new Axis();
+                    ay2.Title = "Отклонение уровня, мм";
+                    chart2.ChartAreas[0].AxisY = ay2;
+                    this.chart1.Series[0].Points.Clear();
+                    var (xResult, yResult) = client.Approximate(paramA, paramB);
+                    for (int i = 0; i < paramA.Length; i++)
+                    {
+                        this.chart1.Series[0].Points.AddXY(paramA[i], paramB[i]);
+                    }
+                    for (int i = 0; i < xResult.Length; i++)
+                    {
+                        this.chart1.Series[1].Points.AddXY(xResult[i], yResult[i]);
+                    }
+                    (xResult, yResult) = client.Approximate(paramA, paramC);
+                    for (int i = 0; i < paramA.Length; i++)
+                    {
+                        this.chart2.Series[0].Points.AddXY(paramA[i], paramC[i]);
+                    }
+                    for (int i = 0; i < xResult.Length; i++)
+                    {
+                        this.chart2.Series[1].Points.AddXY(xResult[i], yResult[i]);
+                    }
+                }
+                catch (System.NullReferenceException)
+                {
+                    MessageBox.Show("Вы не загрузили данные!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                catch (System.FormatException)
+                {
+                    MessageBox.Show("Не верный формат данных!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                catch (System.ServiceModel.EndpointNotFoundException)
+                {
+                    MessageBox.Show("Не удалось подключится к серверу!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                break;
+            } while (true);
+        }
+
+        private void ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Аппроксимация (от лат. proxima — ближайшая) — научный метод, " +
+                            "состоящий в замене одних объектов другими, в каком-то смысле " +
+                            "близкими к исходным, но более простыми. Аппроксимация позволяет " +
+                            "исследовать числовые характеристики и качественные свойства объекта, " +
+                            "сводя задачу к изучению более простых или более удобных объектов " +
+                            "(например, таких, характеристики которых легко вычисляются или " +
+                            "свойства которых уже известны).", "Сведенье", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
